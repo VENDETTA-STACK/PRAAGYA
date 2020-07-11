f523137846fbb4c83b9b3bec9f15a598ca49e6bb
drmehta1996 <dm27091996@gmail.com>
2020-07-11 10:06:32 +0530

    make it better

--- router/locationRouter.js
+++ router/locationRouter.js
@@ -6,6 +6,5 @@ const locationController = require("../controller/locationController");
 
 locationRouter.post("/country", locationController.getCountry);
 locationRouter.post("/state", locationController.getState);
-locationRouter.post("/city", locationController.getCity);
 
 module.exports = locationRouter;
