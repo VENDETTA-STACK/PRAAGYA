96f4a4afeefce5f09af742d219a8222f1585d7eb
Swapnil B <13160478+gitswapnil@users.noreply.github.com>
2020-12-05 16:05:31 +0530

    debugged email required error from validation.

--- controller/userController.js
+++ controller/userController.js
@@ -741,8 +741,7 @@ module.exports = {
 function createUserValidation(user) {
   const schema = Joi.object().keys({
     name: Joi.string().min(5).max(30).required(),
-    email: Joi.string().email({ minDomainAtoms: 2 }).max(30).required(),
-    password: Joi.string().min(6).max(30).required(),
+    mobileNumber: Joi.string().min(10).max(10).required(),
   });
   return Joi.validate(user, schema);
 }
